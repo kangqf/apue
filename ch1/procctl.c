d6a87a9e04cc80c9fcb88363612109e338ab4777
kangqf <kangqingfei@163.com>
2018-04-23 00:24:34 +0800

    add procctl

---
+++ ch1/procctl.c
@@ -0,0 +1,33 @@
+#include "../apue/apue.h"
+#include <sys/wait.h>
+
+int main()
+{
+    char buf[MAXLINE];
+    pid_t pid;
+    int status;
+
+    printf("%%");
+    while(fgets(buf,MAXLINE,stdin) != NULL)
+    {
+        if(buf[strlen(buf) - 1] == '\n')
+            buf[strlen(buf) - 1] = 0;
+        
+        if((pid = fork()) < 0)
+            err_sys("fork error");
+        else if(pid == 0)//child
+        {
+            execlp(buf,buf,(char *) 0);
+            err_ret("could exec %s",buf);
+            exit(127);
+        }
+
+        // parent
+        if((pid = waitpid(pid,&status,0) ) < 0)
+        {
+            err_sys("wait error"); 
+        }
+        printf("%%");
+    }  
+    exit(0);
+}
